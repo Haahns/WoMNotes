215dcff0d82396d190ba6e080e67f50d224fb973
Jakob Hahnsson <hahnssoj@arcada.fi>
2022-09-12 15:41:40 +0300

    Lektion slut 12.9.2022

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 
 // Vi måste importera express-modulen för att skapa en express-app
 const express = require('express')
+const mongoose  = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 3030
 
+require('dotenv').config()
+console.log(process.env.HEMLIS)
+
 app.use(express.json())
+
+console.log(process.env.DB_URL)
+
+mongoose.connect(process.env.DB_URL)
+
+const db = mongoose.connection
+db.on('error', (error) =>  console.log(error))
+db.on('open', () =>  console.log("Connected to DB!"))
+
 
 app.get('/', (req, res) => {
     res.send({msg: `Express says hello!`})
