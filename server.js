f2a2d8c96c60aa58c179dcd39fc545ff371bfb9a
Jakob Hahnsson <hahnssoj@arcada.fi>
2022-09-12 14:25:03 +0300

    mellan push

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3030
 
-/
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send(`Express says hello! ${res.locals.myVariable}`)
+    res.send({msg: `Express says hello!`})
 })
-
+const notesRouter = require('./routes/notes')
+app.use('/notes', notesRouter)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
