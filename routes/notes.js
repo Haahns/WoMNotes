790b8bd9d4dbda6925a09a29f235c679e6060770
Jakob Hahnsson <hahnssoj@arcada.fi>
2022-09-14 15:14:59 +0300

    slutet av lektionen, men måste fixas

--- routes/notes.js
+++ routes/notes.js
@@ -1,24 +1,36 @@
 const express = require('express')
 const router = express.Router()
+const Note = require ("../models/note")
 
-const notes = [
-    {text: "Köp bröd."},
-    {text: "Köp sprit."},
-    {text: "Pumpa cykeln."}
-]
 
-router.get('/', (req, res) => {
-    res.send(notes);
-})
+router.get('/', async (req, res) => {
+    try {
+    const notes = await Note.find()
+    res.send(notes)
+    } catch (error){
+    res.status(500).send({ msg: error.message})}
+});
 
 router.get('/:id', (req, res) => {
     res.send(notes[req.params.id]);
 })
 
 
-router.post('/', (req, res) => {
+/*router.post('/', (req, res) => {
     notes.push(req.body)
     res.send({sparade: req.body})
-})
+})*/
+router.post('/',async (req, res) => {
+    try {
+        const note = new Note ({
+            text: req.body.text
+        })
 
+        
+
+        res.send({ sparade : note})
+    } catch (error){
+        res.status(500).send({ msg: error.message})
+    }
+})
 module.exports = router
--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const mongoose  = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 3030
 
+//importera dotenv och läs in .env-filen
 require('dotenv').config()
 console.log(process.env.HEMLIS)
 
 app.use(express.json())
-
+//använd en variablem ur .env-filen
 console.log(process.env.DB_URL)
 
+//skapa och öppna mongoose-connection till Mongo DB appen
 mongoose.connect(process.env.DB_URL)
-
 const db = mongoose.connection
 db.on('error', (error) =>  console.log(error))
 db.on('open', () =>  console.log("Connected to DB!"))
